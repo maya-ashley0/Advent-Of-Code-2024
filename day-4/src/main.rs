28bd2834acdb377ab7724dfa3c7df78b312190a7
Maya Ashley <maya-git.lend600@passmail.net>
2024-12-05 18:27:07 +0000

    Day 4 part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
+use std::slice::ChunksExact;
 
 fn main() {
     let mut input_file = File::open("src/input.txt").expect("couldn't open file");
@@ -17,11 +18,26 @@ fn main() {
         .read(&mut input_file_contents[..]) // TODO stream file with BufReader?
         .expect("couldn't read file into memory");
 
-    let num_hits: u32 = find_positions_of_xmas(input_file_contents)
-        .into_values()
-        .map(|count| u32::from(count))
-        .sum();
-    println!("found XMAS {} times", num_hits);
+    let length_of_line = input_file_contents
+            .iter()
+            .position(|c| *c == Char::LineFeed as u8)
+            .expect("unable to find a newline") // TODO handle single-line files
+            + 1;
+    let input_lines = input_file_contents.chunks_exact(length_of_line);
+    if !input_lines.remainder().is_empty() {
+        panic!("leftover input after splitting input lines");
+    }
+
+    let num_hits_pt_1: u32 =
+        find_positions_of_xmas(&input_file_contents, input_lines.clone(), length_of_line)
+            .into_values()
+            .map(|count| u32::from(count))
+            .sum();
+    println!("found XMAS {} times", num_hits_pt_1);
+
+    let num_hits_pt_2 =
+        find_positions_of_mas_in_x(&input_file_contents, input_lines, length_of_line);
+    println!("found MAS in X {} times", num_hits_pt_2);
 }
 
 // std::ascii::Char isn't stable yet, so here's what we need from it:
@@ -34,17 +50,35 @@ enum Char {
     CapitalX = 88,
 }
 
-fn find_positions_of_xmas(input_file_contents: Vec<u8>) -> HashMap<(usize, usize), u8> {
-    let length_of_line = input_file_contents
-            .iter()
-            .position(|c| *c == Char::LineFeed as u8)
-            .expect("unable to find a newline") // TODO handle single-line files
-            + 1;
-    let input_lines = input_file_contents.chunks_exact(length_of_line);
-    if !input_lines.remainder().is_empty() {
-        panic!("leftover input after splitting input lines");
+fn find_positions_of_mas_in_x(
+    input_file_contents: &Vec<u8>,
+    input_lines: ChunksExact<'_, u8>,
+    length_of_line: usize,
+) -> usize {
+    let mut results = 0;
+    let mut line_id = 0;
+    let num_lines = input_lines.len();
+    for line in input_lines {
+        // don't use enumerate, since it consumes the iter for ChunksExact
+        let mut c_id = 0;
+        for c in line {
+            if *c == Char::CapitalA as u8 {
+                if input_file_contents.num_mas_in_x_at(num_lines, length_of_line, line_id, c_id) {
+                    results += 1;
+                }
+            }
+            c_id += 1;
+        }
+        line_id += 1;
     }
+    results
+}
 
+fn find_positions_of_xmas(
+    input_file_contents: &Vec<u8>,
+    input_lines: ChunksExact<'_, u8>,
+    length_of_line: usize,
+) -> HashMap<(usize, usize), u8> {
     let mut results = HashMap::new();
     let mut line_id = 0;
     let num_lines = input_lines.len();
@@ -67,6 +101,22 @@ fn find_positions_of_xmas(input_file_contents: Vec<u8>) -> HashMap<(usize, usize
 }
 
 trait CountXmasOccurrences {
+    fn num_mas_in_x_at(
+        &self,
+        num_lines: usize,
+        length_of_line: usize,
+        line_id: usize,
+        c_id: usize,
+    ) -> bool;
+
+    fn look_for_mas_in_x(
+        &self,
+        length_of_line: usize,
+        index: usize,
+        c_direction: isize,
+        line_direction: isize,
+    ) -> bool;
+
     fn num_xmas_at(
         &self,
         num_lines: usize,
@@ -74,9 +124,78 @@ trait CountXmasOccurrences {
         line_id: usize,
         c_id: usize,
     ) -> u8;
+
+    fn look_for_mas(
+        &self,
+        length_of_line: usize,
+        index: usize,
+        c_direction: isize,
+        line_direction: isize,
+    ) -> bool;
 }
 
 impl CountXmasOccurrences for Vec<u8> {
+    // should have made a new wrapping type to hold extra state
+    fn look_for_mas_in_x(
+        &self,
+        length_of_line: usize,
+        index: usize,
+        c_direction: isize,
+        line_direction: isize,
+    ) -> bool {
+        let index = index as isize;
+        let length_of_line = length_of_line as isize;
+        self[(index - 1 * c_direction + length_of_line * -1 * line_direction) as usize]
+            == Char::CapitalM as u8
+            // we already know there's an A here, because that's why we're looking here:
+            //&& self[(index + 0 * c_direction + length_of_line * 0 * line_direction) as usize]
+            //    == Char::CapitalA as u8
+            && self[(index + 1 * c_direction + length_of_line * 1 * line_direction) as usize]
+                == Char::CapitalS as u8
+    }
+
+    fn num_mas_in_x_at(
+        &self,
+        num_lines: usize,
+        length_of_line: usize,
+        line_id: usize,
+        c_id: usize,
+    ) -> bool {
+        let index = calculate_index(length_of_line, line_id, c_id);
+        const REMAINING_LEN: usize = 1; // length of either "M" or "S"
+
+        let room_plus_x = length_of_line - c_id > REMAINING_LEN;
+        let room_minus_x = c_id >= REMAINING_LEN;
+        let room_plus_y = num_lines - line_id > REMAINING_LEN;
+        let room_minus_y = line_id >= REMAINING_LEN;
+
+        if !(room_plus_x && room_plus_y && room_minus_x && room_minus_y) {
+            return false;
+        }
+
+        return (self.look_for_mas_in_x(length_of_line, index, 1, 1)
+            || self.look_for_mas_in_x(length_of_line, index, -1, -1))
+            && (self.look_for_mas_in_x(length_of_line, index, 1, -1)
+                || self.look_for_mas_in_x(length_of_line, index, -1, 1));
+    }
+
+    fn look_for_mas(
+        &self,
+        length_of_line: usize,
+        index: usize,
+        c_direction: isize,
+        line_direction: isize,
+    ) -> bool {
+        let index = index as isize;
+        let length_of_line = length_of_line as isize;
+        self[(index + 1 * c_direction + length_of_line * 1 * line_direction) as usize]
+            == Char::CapitalM as u8
+            && self[(index + 2 * c_direction + length_of_line * 2 * line_direction) as usize]
+                == Char::CapitalA as u8
+            && self[(index + 3 * c_direction + length_of_line * 3 * line_direction) as usize]
+                == Char::CapitalS as u8
+    }
+
     fn num_xmas_at(
         &self,
         num_lines: usize,
@@ -93,67 +212,28 @@ impl CountXmasOccurrences for Vec<u8> {
         let room_plus_y = num_lines - line_id > REMAINING_LEN;
         let room_minus_y = line_id >= REMAINING_LEN;
 
-        if room_plus_x
-            && self[index + 1] == Char::CapitalM as u8
-            && self[index + 2] == Char::CapitalA as u8
-            && self[index + 3] == Char::CapitalS as u8
-        {
+        if room_plus_x && self.look_for_mas(length_of_line, index, 1, 0) {
             results += 1;
         }
-
-        if room_minus_x
-            && self[index - 1] == Char::CapitalM as u8
-            && self[index - 2] == Char::CapitalA as u8
-            && self[index - 3] == Char::CapitalS as u8
-        {
+        if room_minus_x && self.look_for_mas(length_of_line, index, -1, 0) {
             results += 1;
         }
-
-        if room_plus_y
-            && self[index + length_of_line * 1] == Char::CapitalM as u8
-            && self[index + length_of_line * 2] == Char::CapitalA as u8
-            && self[index + length_of_line * 3] == Char::CapitalS as u8
-        {
+        if room_plus_y && self.look_for_mas(length_of_line, index, 0, 1) {
             results += 1;
         }
-
-        if room_minus_y
-            && self[index - length_of_line * 1] == Char::CapitalM as u8
-            && self[index - length_of_line * 2] == Char::CapitalA as u8
-            && self[index - length_of_line * 3] == Char::CapitalS as u8
-        {
+        if room_minus_y && self.look_for_mas(length_of_line, index, 0, -1) {
             results += 1;
         }
-
-        if (room_plus_x && room_plus_y)
-            && self[index + 1 + length_of_line * 1] == Char::CapitalM as u8
-            && self[index + 2 + length_of_line * 2] == Char::CapitalA as u8
-            && self[index + 3 + length_of_line * 3] == Char::CapitalS as u8
-        {
+        if room_plus_x && room_plus_y && self.look_for_mas(length_of_line, index, 1, 1) {
             results += 1;
         }
-
-        if (room_plus_x && room_minus_y)
-            && self[index + 1 - length_of_line * 1] == Char::CapitalM as u8
-            && self[index + 2 - length_of_line * 2] == Char::CapitalA as u8
-            && self[index + 3 - length_of_line * 3] == Char::CapitalS as u8
-        {
+        if room_plus_x && room_minus_y && self.look_for_mas(length_of_line, index, 1, -1) {
             results += 1;
         }
-
-        if (room_minus_x && room_plus_y)
-            && self[index - 1 + length_of_line * 1] == Char::CapitalM as u8
-            && self[index - 2 + length_of_line * 2] == Char::CapitalA as u8
-            && self[index - 3 + length_of_line * 3] == Char::CapitalS as u8
-        {
+        if room_minus_x && room_plus_y && self.look_for_mas(length_of_line, index, -1, 1) {
             results += 1;
         }
-
-        if (room_minus_x && room_minus_y)
-            && self[index - 1 - length_of_line * 1] == Char::CapitalM as u8
-            && self[index - 2 - length_of_line * 2] == Char::CapitalA as u8
-            && self[index - 3 - length_of_line * 3] == Char::CapitalS as u8
-        {
+        if room_minus_x && room_minus_y && self.look_for_mas(length_of_line, index, -1, -1) {
             results += 1;
         }
 
@@ -171,12 +251,26 @@ mod tests {
     use crate::CountXmasOccurrences;
 
     #[test]
-    fn test_search_does_not_panic() {
+    fn test_search_xmas_does_not_panic() {
         let input = vec![Char::CapitalX as u8; 5 * 5];
         let mut count = 0;
         for line in 0..5 {
             for char in 0..5 {
                 count += input.num_xmas_at(5, 5, line, char);
+            }
+        }
+        assert_eq!(0, count);
+    }
+
+    #[test]
+    fn test_search_mas_does_not_panic() {
+        let input = vec![Char::CapitalM as u8; 5 * 5];
+        let mut count = 0;
+        for line in 0..5 {
+            for char in 0..5 {
+                if input.num_mas_in_x_at(5, 5, line, char) {
+                    count += 1;
+                }
             }
         }
         assert_eq!(0, count);
