5f20508c4fcc3fa01a1b791365856d293ca874bb
Maya Ashley <maya-git.lend600@passmail.net>
2024-12-05 18:27:07 +0000

    Day 2 Part 2
    This solution feels bad, but idk enough math to come up with a cleaner one

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::io::Read;
 
 use nom;
-use nom::character::is_newline;
 
 fn main() {
     let mut input_file = File::open("src/input.txt").expect("couldn't open file");
@@ -16,11 +15,24 @@ fn main() {
     // Any two adjacent levels differ by at least one and at most three.
 
     let input = parse_level_reports(&input_file_contents);
+
     let num_safe: i16 = input
         .iter()
         .map(|report| if determine_safety(report) { 1 } else { 0 })
         .sum();
     println!("num_safe = {}", num_safe);
+
+    let num_dampened_safe: i16 = input
+        .iter()
+        .map(|report| {
+            if determine_dampened_safety(report) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum();
+    println!("num_dampened_safe = {}", num_dampened_safe);
 }
 
 fn determine_safety(report: &Vec<i16>) -> bool {
@@ -36,6 +48,53 @@ fn determine_safety(report: &Vec<i16>) -> bool {
         }
     }
     return true;
+}
+
+struct DampenedPossibilities {
+    report: Vec<i16>,
+    iteration: usize,
+}
+
+impl DampenedPossibilities {
+    fn new(report: &Vec<i16>) -> DampenedPossibilities {
+        DampenedPossibilities {
+            report: report.clone(),
+            iteration: 0,
+        }
+    }
+}
+
+impl std::iter::Iterator for DampenedPossibilities {
+    type Item = Vec<i16>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iteration += 1;
+        if self.iteration == 1 {
+            return Some(self.report.clone());
+        }
+        if (self.iteration - 1) > self.report.len() {
+            return None;
+        }
+        let skip_index = self.iteration - 2;
+        return Some(
+            self.report
+                .clone()
+                .into_iter()
+                .enumerate()
+                .filter_map(|(i, level)| if i != skip_index { Some(level) } else { None })
+                .collect(),
+        );
+    }
+}
+
+fn determine_dampened_safety(report: &Vec<i16>) -> bool {
+    let possibilities = DampenedPossibilities::new(report);
+    for possibility in possibilities {
+        if determine_safety(&possibility) {
+            return true;
+        }
+    }
+    return false;
 }
 
 // TODO add error handling
@@ -61,11 +120,15 @@ fn parse_levels(input: &str) -> nom::IResult<&str, Vec<i16>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::determine_safety;
+    use crate::determine_dampened_safety;
 
     #[test]
-    fn test_determine_safety() {
-        assert_eq!(determine_safety(&vec![7, 6, 4, 2, 1]), true);
-        assert_eq!(determine_safety(&vec![1, 2, 7, 8, 9]), false);
+    fn test_determine_dampened_safety() {
+        assert_eq!(determine_dampened_safety(&vec![7, 6, 4, 2, 1]), true);
+        assert_eq!(determine_dampened_safety(&vec![1, 2, 7, 8, 9]), false);
+        assert_eq!(determine_dampened_safety(&vec![9, 7, 6, 2, 1]), false);
+        assert_eq!(determine_dampened_safety(&vec![1, 3, 2, 4, 5]), true);
+        assert_eq!(determine_dampened_safety(&vec![8, 6, 4, 4, 1]), true);
+        assert_eq!(determine_dampened_safety(&vec![1, 3, 6, 7, 9]), true);
     }
 }
